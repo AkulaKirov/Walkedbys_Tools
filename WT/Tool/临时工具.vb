d7baaf3b1abfb5066bbd4dc705e41545e4a1b8d7
Gordon Walkedby <gordonwalkedby@gmail.com>
2018-09-21 18:52:43 +0800

    0921

--- WT/Tool/临时工具.vb
+++ WT/Tool/临时工具.vb
@@ -1,4 +1,7 @@
 ﻿
 Public Class 临时工具
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim b As Image = 剪贴板.图片
+        If Not IsNothing(b) Then b.Save("e:\1.jpg")
+    End Sub
 End Class
