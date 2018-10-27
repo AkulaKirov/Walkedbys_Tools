30a0cb3491eea8472fd56092fdc9def30de36042
走过去 戈登 <gordonwalkedby@gmail.com>
2018-10-27 15:15:54 +0800

    v0.20

--- WT/ApplicationEvents.vb
+++ WT/ApplicationEvents.vb
@@ -12,7 +12,7 @@ Namespace My
             ElseIf i.EndsWith("\desktop\") Then
                 报错退出("对不起，为了桌面的美观，请不要在桌面直接运行本程序。")
             End If
-            If 版本转小数(Environment.OSVersion.Version) < 6.09 Then 报错退出("对不起，本程序不支持低于 Windows 7 的系统。")
+            If 版本转小数(Environment.OSVersion.Version) < 6.09 Then 报错退出("对不起，本程序不支持低于 Windows 7 Service Pack 1 的系统。")
             i = Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full", "Release", "")
             If Val(i) < 460000 Then 报错退出("对不起，请安装 .NET Framework 4.7 或以上版本。")
             i = 程序文件名
