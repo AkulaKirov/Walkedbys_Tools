a05f51eddadbfdd84adca5fb6359dc81075a6b4c
走过去 戈登 <gordonwalkedby@gmail.com>
2018-11-05 21:17:26 +0800

    不再限制 .NET 4.7

--- WT/ApplicationEvents.vb
+++ WT/ApplicationEvents.vb
@@ -6,20 +6,27 @@ Namespace My
 
         Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
             Dim i As String
+            i = 程序文件名
+            If i <> "wt" Then 报错退出("对不起，你不能乱修改文件。
+请尝试重新安装本程序。")
+            If Process.GetProcessesByName("wt").Count > 1 Then 报错退出("对不起，本程序不能多开。
+或者请把其他名为 wt 的进程退出。")
             i = 程序文件目录.ToLower
             If 全部包含(i, "c:\users", "\appdata\", "\temp\") Then
                 报错退出("对不起，请不要把本程序在压缩包内直接打开。")
             ElseIf i.EndsWith("\desktop\") Then
                 报错退出("对不起，为了桌面的美观，请不要在桌面直接运行本程序。")
             End If
-            If 版本转小数(Environment.OSVersion.Version) < 6.09 Then 报错退出("对不起，本程序不支持低于 Windows 7 Service Pack 1 的系统。")
+            If 版本转小数(Environment.OSVersion.Version) < 6.09 Then
+                报错退出("对不起，本程序不支持低于 Windows 7 Service Pack 1 的系统。")
+            End If
             i = Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full", "Release", "")
-            If Val(i) < 460000 Then 报错退出("对不起，请安装 .NET Framework 4.7 或以上版本。")
-            i = 程序文件名
-            If i <> "wt" Then 报错退出("对不起，你不能乱修改文件。
-请尝试重新安装本程序。")
-            If Process.GetProcessesByName("wt").Count > 1 Then 报错退出("对不起，本程序不能多开。
-或者请把其他名为 wt 的进程退出。")
+            If Val(i) < 460000 Then
+                MsgBox("你的 .NET Framework 虽然已经达到了运行程序的要求，
+但是要完美运行本程序，
+请安装 .NET Framework 4.7 或以上版本。
+如果你不安装，每次开启本程序时都会弹出本提示。", MsgBoxStyle.Information, "出错！走過去的工具箱 v" + 版本.ToString)
+            End If
             For Each i In e.CommandLine
                 启动参数.Add(i.ToLower)
             Next
